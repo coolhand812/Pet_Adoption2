c3617a26e22aac6311178d63bacf6c832c83f26a
Joe Diaz <31023058+coolhand812@users.noreply.github.com>
2018-06-10 01:22:29 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<title>Joe's Website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>Joe's Website</h1>
+			</div>
+			
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li><a href="main_menu.html">Main</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="mailto:coolhand812@cityuniversity.edu">Contact</a></li>
+					</ul>
+				</div>
+				
+				<div id="header2">
+					<h2>We are your pet shelter!</h2>
+				</div>
+				
+				<div id="main">
+						
+				<p>...and, we are passionate about our canine friends.</p>
+                <p> It is no secret that our canine companions enrich our lives </p>
+                <p> and, in order to prolong our best friend's life, we need to keep them healthy. </p>
+                <hr>
+                <p><b>Why adopt instead of buyng your pet?</b></p>
+                    <ol>
+                        <li>Because you'll save a life</li>
+                        <li>Because you'll get a great animal</li>
+                        <li>Because it'll cost you less</li>
+						<li>Because it's one way to fight puppy mills</li>
+						<li>Because you'll change a homeless animal's whole world</li>
+                    </ol>
+
+                <p><b>What are some of the benefits of waking your dog?</b></p>
+                    <ol>
+                        <li>Keeps their heart and lungs in top shape</li>
+                        <li>It keeps their joints healthy</li>
+                        <li>It helps their digestive and circulatory system</li>
+                        <li>It stops their destructive behavior and hyperactivity</li>
+                    </ol>
+                <hr>	
+					
+				</div>
+				
+			</div>
+			
+			<div id="footer">
+				Copyright &copy; 2018 Joe Diaz
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
---
+++ list_pets.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+		<title>Joe's Website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>Joe's Website</h1>
+			</div>
+			
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li><a href="main_menu.html">Main</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="mailto:coolhand812@cityuniversity.edu">Contact</a></li>
+					</ul>
+				</div>
+				
+				<div id="header2">
+					<h2>Available Pet List</h2>
+				</div>
+				
+				<div id="main">
+				<br><br>
+				<?php
+					//generate db query and get a table
+					$servername = "localhost";
+					$username = "root";
+					$password = "";
+					$dbname = "pet_adoption";
+
+					// Create connection
+					$conn = new mysqli($servername, $username, $password, $dbname);
+					// Check connection
+					if ($conn->connect_error) {
+    					die("Connection failed: " . $conn->connect_error);
+					} 
+
+					$sql = "SELECT pet_id, name, gender, age, type_id, notes, intake_date FROM pettable";
+					$result = $conn->query($sql);
+
+					if ($result->num_rows > 0) {
+    					// output data of each row
+    					while($row = $result->fetch_assoc()) {
+        				echo "id: " . $row["pet_id"]. " - Name: " . $row["name"]. " - Gender: " . $row["gender"]. " - Age: " . $row["age"]. " - Type: " . $row["type_id"]. " - Notes: " . $row["notes"]. " - Intake Date: " . $row["intake_date"]. "<br>";
+    					}
+					} else {
+    					echo "0 results";
+					}
+					$conn->close();
+				?>	
+				</br></br>
+				<a href="main_menu.html">Go back to the main page</a>	
+				</div>
+				
+			</div>
+			
+			<div id="footer">
+				Copyright &copy; 2018 Joe Diaz
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
