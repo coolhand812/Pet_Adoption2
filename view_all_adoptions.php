265a63cc1e3d4fa34f118fb7c12b40f7361cd817
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2018-06-11 22:11:13 -0500

    coded the 3 table join

--- view_all_adoptions.php
+++ view_all_adoptions.php
@@ -30,17 +30,26 @@
 					//generate db query and get a table
 					include 'db_connection.php'; 
 
-					$sql = "SELECT pet_id, name, gender, age, type_id, notes, intake_date FROM pettable";
-					$result = $conn->query($sql);
+                    $join = ("SELECT pettable.pet_id
+                                     pettable.name
+                                     pettable.gender
+                                     pettable.intake_date
+                                     pet_typetable.pet_type as ptype
+                                     adoption.date_of_adoption as date_adopted
+                            FROM pettable
+                            INNER JOIN adoption on adoption.pet_id = pettable.pet_id
+                            INNER JOIN pet_typetable on pet_typetable.pet_type = pettable.type_id");
+                    $result = $conn->query($join);
 
 					if ($result->num_rows > 0) {
     					// output data of each row
     					while($row = $result->fetch_assoc()) {
-        				echo "id: " . $row["pet_id"]. " - Name: " . $row["name"]. " - Gender: " . $row["gender"]. " - Age: " . $row["age"]. " - Type: " . $row["type_id"]. " - Notes: " . $row["notes"]. " - Intake Date: " . $row["intake_date"]. "<br>";
+        				echo "id: " . $row["pet_id"]. " - Name: " . $row["name"]. " - Gender: " . $row["gender"]. " - IntakeDate: " . $row["intake_date"]. " - Type: " . $row["pet_type"]. " - Date adopted: " . $row["date_of_adoption"]. "<br>";
     					}
 					} else {
     					echo "0 results";
-					}
+					}        
+                    
 					$conn->close();
 				?>	
 				</br></br>
